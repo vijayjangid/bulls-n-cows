83a5c97e2a6939ea9740b3619800e1eddcf8332c
Vijay Jangid <vijskt@gmail.com>
2021-05-12 16:21:19 +0530

    fixed errors

--- src/state/game-stats-context.js
+++ src/state/game-stats-context.js
@@ -1,17 +1,29 @@
-import { createContext, useCallback, useEffect, useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState
+} from "react";
 import { MIN_DIGITS } from "../constants";
+import { SecretContext } from "./secret-context";
 
 export const GameStatsContext = createContext();
 const initialState = {
   digits: MIN_DIGITS,
   totalGames: 0,
-  wins: 0,
-  losses: 0,
   bestScore: 0,
   avgScore: 0
 };
 export const GameStatsContextProvider = ({ children }) => {
+  const { secretSize } = useContext(SecretContext);
   const [gameStats, setGameStats] = useState(initialState);
+
+  useEffect(() => {
+    const newGameStats = { ...initialState, digits: secretSize };
+    setGameStats(newGameStats);
+  }, [secretSize]);
+
   const resetGameStats = useCallback(() => {
     setGameStats(initialState);
   }, [setGameStats]);
