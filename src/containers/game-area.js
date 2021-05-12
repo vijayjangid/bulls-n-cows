abf957c53ce125d0a401ce73294792e18442516b
Vijay Jangid <vijskt@gmail.com>
2021-05-12 14:01:15 +0530

    fixed state flow

--- src/containers/game-area.js
+++ src/containers/game-area.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useReducer, useState } from "react";
 import { Guess, Keyboard, HintsBoard } from "../components";
 import { useSecret } from "../hooks";
 import { GAME_RESULT } from "../constants";
@@ -19,30 +19,62 @@ function prepareHint(guess, secret) {
   return `${guess.join("")}: ${bulls}B ${cows}C`;
 }
 
+const ACTIONS = {
+  SET_GUESS: "SET_GUESS",
+  SET_HINTS: "SET_HINTS",
+  SET_RESULT: "SET_RESULT",
+  RESET: "RESET"
+};
+
+const initialState = {
+  guess: [],
+  hints: [],
+  result: GAME_RESULT.IN_PROGRESS
+};
+
+function stateReducer(state, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case ACTIONS.RESET:
+      console.log("resetting");
+      return initialState;
+    case ACTIONS.SET_GUESS:
+      return { ...state, guess: payload };
+    case ACTIONS.SET_HINTS:
+      return { ...state, hints: [payload, ...state.hints] };
+    case ACTIONS.SET_RESULT:
+      return { ...state, result: payload };
+
+    default:
+      return state;
+  }
+}
+function setGuess(payload) {
+  return { type: ACTIONS.SET_GUESS, payload };
+}
+function setHints(payload) {
+  return { type: ACTIONS.SET_HINTS, payload };
+}
+function setResult(payload) {
+  return { type: ACTIONS.SET_RESULT, payload };
+}
+function reset() {
+  return { type: ACTIONS.RESET };
+}
 export default function GameArea() {
   const { secret, resetSecret } = useSecret();
-  const [guess, setGuess] = useState([]);
-  const [hints, setHints] = useState([]);
-  const [result, setResult] = useState(GAME_RESULT.IN_PROGRESS);
-
+  const [{ guess, hints, result }, dispatch] = useReducer(
+    stateReducer,
+    initialState
+  );
+  console.log(guess);
   useEffect(() => {
-    setGuess([]);
-    setHints([]);
-    setResult(GAME_RESULT.IN_PROGRESS);
+    dispatch(reset());
   }, [secret]);
 
   useEffect(() => {
-    if (guess.length === secret.length) {
-      const hint = prepareHint(guess, secret);
-      setHints((hs) => [hint, ...hs]);
-    } else if (guess.length > secret.length) {
-      setGuess(guess.slice(secret.length));
-    }
-  }, [guess, secret]);
-
-  useEffect(() => {
     if (secret === guess.join("")) {
-      setResult(GAME_RESULT.WON);
+      dispatch(setResult(GAME_RESULT.WON));
     }
   }, [guess, secret]);
 
@@ -50,7 +82,7 @@ export default function GameArea() {
     if (result === GAME_RESULT.WON) {
       resetSecret();
     } else {
-      setGuess([]);
+      dispatch(setGuess([]));
     }
   }, [result, resetSecret]);
 
@@ -60,13 +92,22 @@ export default function GameArea() {
       if (result !== GAME_RESULT.IN_PROGRESS) {
         return;
       }
-      setGuess((g) => [...g, letter]);
+      let guessIncludingCurrentLetter = [...guess, letter];
+      if (guessIncludingCurrentLetter.length === secret.length) {
+        const hint = prepareHint(guessIncludingCurrentLetter, secret);
+        dispatch(setHints([hint, ...hints]));
+      }
+      guessIncludingCurrentLetter.length > secret.length
+        ? dispatch(setGuess(guessIncludingCurrentLetter.slice(secret.length)))
+        : dispatch(setGuess(guessIncludingCurrentLetter));
     },
-    [result]
+    [result, guess, secret, hints]
   );
+  console.log("hints", hints);
   const disabledKeys = result === GAME_RESULT.WON ? "0123456789".split("") : [];
   return (
     <div className="game-area">
+      {secret}
       <Result result={result} score={hints.length} />
       <Guess guess={guess} size={secret.length} />
       <div className="board-wrapper">
